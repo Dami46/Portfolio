6d9089a96172e5ff1335e4678722f9b47b9efec3
Damian Urbaniak <damian.urbaniak@mikronika.com.pl>
2022-05-16 14:54:40 +0200

    create models

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,21 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HeaderComponent} from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
     NgbModule,
     HttpClientModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule {
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {BioService} from '../services/bio.service';
+import {HeaderService} from '../services/header.service';
+import {Bio} from "../models/bio";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  menuItems = [
+    {title: 'About Me', homePath: '/', fragment: 'about', pagePath: '/about'},
+    {title: 'My Projects', homePath: '/', fragment: 'projects', pagePath: '/projects'},
+    {title: 'My Blog', homePath: '/blog', fragment: '', pagePath: '/blog'}
+  ];
+
+  isHome: Promise<boolean>;
+  bio: Promise<Bio>;
+
+  constructor(private bioService: BioService, private headerService: HeaderService) {
+    this.bio = this.bioService.getBio();
+    this.isHome = this.headerService.isHome();
+  }
+}
